3f52595b9f8c5fa4a0e843297219fd6d07cb18fc
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-06-14 00:41:04 +0800

    Basic CLI

---
+++ src/cli.ts
@@ -0,0 +1,45 @@
+import { Command } from "commander";
+import { Service } from "./backend.js";
+import inquirer from "inquirer";
+import { prompts } from "./inquirer-prompts.js";
+
+const program = new Command();
+
+program
+    .name("mcpm")
+    .description("")
+    .action(() => {
+        console.log(
+            "A Lucy installation was not detected in the current directory."
+        );
+        inquirer.prompt(prompts.initConfirmation).then((answers) => {
+            if (answers.confirm) {
+                Service.getService().initialization();
+            }
+        });
+    });
+
+program
+    .command("init")
+    .description("")
+    .action(() => {});
+
+program.command("list");
+
+program.command("install");
+
+program.command("remove");
+
+program.command("update");
+
+program.command("disable");
+
+program.command("enable");
+
+program.command("export");
+
+program.command("import");
+
+program.command("host");
+
+export { program };
