db54684a86adb046011acc2ece23d02fb913e082
driver1748 <2356747603@qq.com>
2024-07-26 12:16:19 +0800

    chore: Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,8 @@ import tseslint from "typescript-eslint";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
