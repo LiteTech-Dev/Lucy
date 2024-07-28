d658a14436ceb20646e70009900c0b49ecb87955
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-07-28 16:18:15 +0800

    App Data and Cache

--- src/fileUtils/cacheService.ts
+++ src/fileUtils/cacheService.ts
@@ -1,1 +1,52 @@
+import fs from "fs";
+import { tmpdir } from "os";
+import path from "path";
 
+export class CacheService {
+    private static _instance: CacheService;
+    private readonly cachePath = path.join(tmpdir(), "lucy");
+
+    private constructor() {}
+
+    public static getInstance(): CacheService {
+        if (!CacheService._instance)
+            CacheService._instance = new CacheService();
+        return CacheService._instance;
+    }
+
+    public createCacheDirectory(): void {
+        if (!fs.existsSync(this.cachePath)) {
+            fs.mkdirSync(this.cachePath);
+        }
+    }
+
+    public async readFrom(target: string): Promise<string> {
+        const targetPath = path.join(this.cachePath, target);
+        return new Promise((resolve, reject) => {
+            fs.readFile(targetPath, "utf-8", (err, data) => {
+                if (err) reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    public async writeTo(target: string, data: string): Promise<void> {
+        const targetPath = path.join(this.cachePath, target);
+        return new Promise((resolve, reject) => {
+            fs.writeFile(targetPath, data, (err) => {
+                if (err) reject(err);
+                resolve();
+            });
+        });
+    }
+
+    public async clearCache(): Promise<void> {
+        // A delete method was not implemented because no specific file should be deleted from the cache
+        return new Promise((resolve, reject) => {
+            fs.rmdir(this.cachePath, { recursive: true }, (err) => {
+                if (err) reject(err);
+                resolve();
+            });
+        });
+    }
+}
