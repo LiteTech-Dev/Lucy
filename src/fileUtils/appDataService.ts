dacae637db1b41a2f0e8bba14853f0a7b4550dde
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-07-28 16:57:46 +0800

    Basic File Operations (#2)
    * Start working on SHA1-based version checking
    
    * Start new brach from here @KuromeSama6
    
    * Update README.md
    
    * Reconstruct Backend
    
    * Started working on fileUtil.ts
    
    * Fix eslint
    
    * Separation
    
    * App Data and Cache
    
    * Server File Service
    
    ---------
    
    Co-authored-by: KuromeSama6 <harrycgr@126.com>

---
+++ src/fileUtils/appDataService.ts
@@ -0,0 +1,64 @@
+import path from "path";
+import fs from "fs";
+import { cwd } from "process";
+
+export class AppDataService {
+    private static _instance: AppDataService;
+    private readonly appDataPath = path.join(cwd(), ".lucy");
+
+    private constructor() {}
+
+    public static getInstance(): AppDataService {
+        if (!AppDataService._instance)
+            AppDataService._instance = new AppDataService();
+        return AppDataService._instance;
+    }
+
+    public createAppDataPath(): void {
+        if (!fs.existsSync(this.appDataPath)) {
+            fs.mkdirSync(this.appDataPath);
+        }
+    }
+
+    public appDataPathExists(): boolean {
+        return fs.existsSync(this.appDataPath);
+    }
+
+    public fileExists(target: string): boolean {
+        const targetPath = path.join(this.appDataPath, target);
+        return fs.existsSync(targetPath);
+    }
+
+    public async readFrom(target: string): Promise<string> {
+        if (!this.fileExists(target)) return "";
+        const targetPath = path.join(this.appDataPath, target);
+        return new Promise((resolve, reject) => {
+            fs.readFile(targetPath, "utf-8", (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    public async writeTo(target: string, data: string): Promise<void> {
+        // This method overwrites the file if it already exists
+        const targetPath = path.join(this.appDataPath, target);
+        return new Promise((resolve, reject) => {
+            fs.writeFile(targetPath, data, (err) => {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    }
+
+    public async delete(target: string): Promise<void> {
+        if (!this.fileExists(target)) return;
+        const targetPath = path.join(this.appDataPath, target);
+        return new Promise((resolve, reject) => {
+            fs.unlink(targetPath, (err) => {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    }
+}
