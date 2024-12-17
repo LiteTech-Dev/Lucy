adcf525ed5eab457a621819b885b06f3a57ef625
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-12-17 10:39:30 +0800

    refactor: Redesign data structure for ServerInfo

--- types/server_files.go
+++ types/server_files.go
@@ -2,14 +2,18 @@ package types
 
 // ServerInfo components that do not exist, use an empty string
 type ServerInfo struct {
-	HasMcdr        bool
-	McdrConfigPath string
-
-	ServerWorkPath  string
-	ModPath         string
+	HasMcdr         bool
+	McdrConfigPath  string
 	McdrPluginPaths []string
-	ExecutablePath  string
 
+	ServerWorkPath string
+	ModPath        string
+
+	Executable ServerExecutable
+}
+
+type ServerExecutable struct {
+	Path             string
 	GameVersion      string
 	ModLoaderType    string
 	ModLoaderVersion string
