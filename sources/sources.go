71b3d8c6b1b7f81dc7345e49af5abf5291a5a9cf
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-12-26 18:38:06 +0800

    wip: Add concurrency in speed test

--- sources/sources.go
+++ sources/sources.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"lucy/syntax"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -27,25 +28,34 @@ var SpeedTestUrls = map[Source]string{
 	Modrinth:   "https://cdn.modrinth.com/data/P7dR8mSH/versions/nyAmoHlr/fabric-api-0.87.2%2B1.19.4.jar",
 }
 
-const slow = 2e31
+const slow float64 = 0x7FF0000000000000 // inf
 
 func SelectSource(platform syntax.Platform) Source {
 	min := slow
 	fastestSource := None
+	wg := sync.WaitGroup{}
 	for _, source := range AvailableSources[platform] {
-		speed := testDownloadSpeed(SpeedTestUrls[source])
-		if speed < min {
-			fastestSource = source
-		}
-		fmt.Printf("Speed for %s: %f\n", source, speed)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			speed := testDownloadSpeed(SpeedTestUrls[source])
+			if speed < min {
+				fastestSource = source
+			}
+			fmt.Printf("Speed for %s: %f\n", source, speed)
+		}()
 	}
+
+	wg.Wait()
 	if fastestSource == None {
 		panic("No available source")
 	}
+
 	return fastestSource
 }
 
 func testDownloadSpeed(url string) (elapsedTime float64) {
+	startTime := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
 		return slow
@@ -53,16 +63,13 @@ func testDownloadSpeed(url string) (elapsedTime float64) {
 	defer resp.Body.Close()
 
 	chunkSize := 2048
-	startTime := time.Now()
 
 	buffer := make([]byte, chunkSize)
-	totalDownloaded := 0
 	for i := 0; i < 10; i++ {
 		n, err := resp.Body.Read(buffer)
 		if err != nil && err != io.EOF {
 			return slow
 		}
-		totalDownloaded += n
 		if n == 0 {
 			break
 		}
