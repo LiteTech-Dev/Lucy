9a358d3de2d75e7d7120f4e30477e0586ce68cfd
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-12-30 22:58:21 +0800

    wip(sources): Add multi-url download with competition

--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
 	"context"
 	"lucy/cmd"
 	"lucy/logger"
-	"lucy/sources"
-	"lucy/syntax"
 	"os"
 )
 
 func main() {
-	sources.SelectSource(syntax.Fabric)
 	cmd.Cli.Run(context.Background(), os.Args)
 	logger.WriteAll()
 }
