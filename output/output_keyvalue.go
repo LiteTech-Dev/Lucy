a0dbfc3d9c2c3ce4fe93cd910833ed8291171774
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2025-01-27 16:21:52 +0800

    wip(output): Add formatting functions

--- output/output_keyvalue.go
+++ output/output_keyvalue.go
@@ -21,6 +21,14 @@ func annot(annotation string) {
 	fmt.Fprintf(keyValueWriter, "\t%s", tools.Dim(annotation))
 }
 
+func dim(value string) {
+	fmt.Fprintf(keyValueWriter, "%s", tools.Dim(value))
+}
+
 func newLine() {
 	fmt.Fprintf(keyValueWriter, "\n")
+}
+
+func tab() {
+	fmt.Fprintf(keyValueWriter, "\t")
 }
